ea9021e7eb655f0b329ac541df911c50f08acb28
Senni Kaipio <senni.kaipio@nosto.com>
2016-10-25 14:46:30 +0300

    Move deploy task from stage specific file to generic config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,11 @@
 set :application, 'lastfmtelebot'
 set :repo_url, 'git@github.com:skaipio/lastfmtelebot.git'
 set :user, 'lastfmtelebot'
+
+namespace :deploy do
+  task :restart do
+    on roles(:app) do
+      execute "./deploy.sh restart"
+    end
+  end
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,11 +2,3 @@ set :stage, :production
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 role :app, %w{lastfmtelebot@lastfmtelebot.tk}
-
-namespace :deploy do
-  task :restart do
-    on roles(:app) do
-      execute "./deploy.sh restart"
-    end
-  end
-end
