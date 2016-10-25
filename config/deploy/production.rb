82ad6706d79463d2e6c8992c2df11bc47f796f88
Senni Kaipio <senni.kaipio@nosto.com>
2016-10-25 14:34:24 +0300

    Fix deploy directory path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-set :deploy_to, "/home/#{fetch(:application)}"
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 role :app, %w{lastfmtelebot@lastfmtelebot.tk}
 
