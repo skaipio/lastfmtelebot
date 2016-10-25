c25430935bc7a0d3d6ea3ff9f6d799aea67c0d56
Senni Kaipio <senni.kaipio@nosto.com>
2016-10-25 14:39:45 +0300

    Fix brackets in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 command=$1
 pid=`ps -U $USER -o pid,cmd | sed -nr 's/([0-9]+) python api.py/\1/p'"`
 
-if [ -n "${pid}" && ("$command" -eq "restart" || "$command" -eq "kill") ]; then
+if [[ -n "${pid}" && ("$command" -eq "restart" || "$command" -eq "kill") ]]; then
     kill -TERM `pid`
 fi
 
-if [ "$command" -eq "restart" || "$command" -eq "start" ]; then
+if [[ "$command" -eq "restart" || "$command" -eq "start" ]]; then
     nohup python api.py > /dev/null &
 fi
