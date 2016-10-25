c5658f5c4a4ec2e63fef0af1ffc5599c60e536b5
Senni Kaipio <senni.kaipio@nosto.com>
2016-10-25 14:30:30 +0300

    Add deploy files

---
+++ config/deploy.rb
@@ -0,0 +1,3 @@
+set :application, 'lastfmtelebot'
+set :repo_url, 'git@github.com:skaipio/lastfmtelebot.git'
+set :user, 'lastfmtelebot'
---
+++ config/deploy/production.rb
@@ -0,0 +1,12 @@
+set :stage, :production
+set :deploy_to, "/home/#{fetch(:application)}"
+
+role :app, %w{lastfmtelebot@lastfmtelebot.tk}
+
+namespace :deploy do
+  task :restart do
+    on roles(:app) do
+      execute "./deploy.sh restart"
+    end
+  end
+end
