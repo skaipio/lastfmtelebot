ebf658959925554e29d6b95f312b71549854ded9
Senni Kaipio <senni.kaipio@nosto.com>
2016-11-01 14:12:44 +0200

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,3 @@
 set :application, 'lastfmtelebot'
 set :repo_url, 'git@github.com:skaipio/lastfmtelebot.git'
 set :user, 'lastfmtelebot'
-
-namespace :deploy do
-  task :restart do
-    on roles(:app) do
-      execute "sudo systemctl restart lastfmtelebot.service"
-    end
-  end
-
-  after :published, 'deploy:restart'
-
-end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,21 @@ set :stage, :production
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 role :app, %w{lastfmtelebot@lastfmtelebot.tk}
+
+namespace :deploy do
+  task :install_dependencies do
+    on roles(:app) do
+      execute "source /home/lastfmtelebot/.virtualenvs/botapi/bin/activate && cd /home/lastfmtelebot/lastfmtelebot/current && pip install -r requirements.txt"
+    end
+  end
+
+  task :restart do
+    on roles(:app) do
+      execute "sudo systemctl restart lastfmtelebot.service"
+    end
+  end
+
+  after :published, 'deploy:install_dependencies'
+  after :install_dependencies, 'deploy:restart'
+
+end
