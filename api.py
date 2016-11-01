e83ebdb5057ccf168d4e6739598ef00eacf1273d
Senni Kaipio <senni.kaipio@nosto.com>
2016-11-01 15:51:04 +0200

    Add a telegram bot api client

--- api.py
+++ api.py
@@ -1,12 +1,22 @@
 from flask import Flask, redirect, request, session
 from lastfm_client import LastFMClient
+from telegram_botapi_client import TelegramBotAPIClient
 
 app = Flask(__name__)
 app.config.from_envvar('LASTFMBOTAPI_CONFIG')
 
 session_map = {}
 
+TELEGRAM_BOT_KEY=app.config['TELEGRAM_BOT_KEY']
+TELEGRAM_BOT_TOKEN=app.config['TELEGRAM_BOT_TOKEN']
+TELEGRAM_BOTAPI_CLIENT = TelegramBotAPIClient(TELEGRAM_BOT_KEY, TELEGRAM_BOT_TOKEN)
 LASTFM_CLIENT = LastFMClient(app.config['LASTFM_API_KEY'], app.config['LASTFM_SECRET'])
+
+@app.route("/")
+def bot_query():
+    token = get_token(request)
+    if token == TELEGRAM_BOT_TOKEN:
+        print request.content
 
 @app.route("/session")
 def new_session():
---
+++ telegram_botapi_client.py
@@ -0,0 +1,32 @@
+import requests
+
+class TelegramBotAPIClient(object):
+    BOT_API_ROOT='https://api.telegram.org'
+
+    def __init__(self, bot_key, bot_token):
+        self.bot_key = bot_key
+        self.bot_token = bot_token
+        if self.__get_bot != None:
+            self.__set_webhook()
+
+    def __get_bot(self):
+        response = self.__do_get_request('getMe')
+        json = response.json()
+        if json['ok']==True:
+            return json['result']
+        return None
+
+    def __set_webhook(self):
+        webhook_url="https://lastfmtelebot.tk/{bot_token}".format(bot_token=self.bot_token)
+        json = self.__do_get_request('setWebhook', {'url': webhook_url}).json()
+        if json['ok']==True:
+            return json.result
+        return None
+
+    def __do_get_request(self, method, params={}):
+        url = "{root}/bot{bot_key}/{method}".format(root=self.BOT_API_ROOT, bot_key=self.bot_key, method=method)
+        return requests.get(url, params)
+
+    def __do_post_request(self, method, params):
+        url = "{root}/bot{bot_key}/{method}".format(root=self.BOT_API_ROOT, bot_key=self.bot_key, method=method)
+        return requests.post(url,params=params)
